b6550adc8b22d5a3fd8a1a512f55959cea687898
webAqemu <notych2812@gmail.com>
2020-05-16 14:17:24 +0300

    added autoplay to first slider

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ $(document).ready(function () {
       el: '.swiper-pagination',
       clickable: true,
     },
+    autoplay: {
+      delay: 3000,
+    },
   });
 
   var mySwiper = new Swiper('.instagram__swiper-container', {
